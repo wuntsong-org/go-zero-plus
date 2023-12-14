da4aeb8a3608f954650805f6bb7bfc89b75b321e
SongZihuan <songzihuan@song-zh.com>
2023-12-14 17:07:24 +0800

    fix: 删除goctl

--- tools/goctlwt/bug/cmd.go
+++
@@ -1,9 +0,0 @@
-package bug
-
-import (
-	"github.com/spf13/cobra"
-	"github.com/wuntsong-org/go-zero-plus/tools/goctlwt/internal/cobrax"
-)
-
-// Cmd describes a bug command.
-var Cmd = cobrax.NewCommand("bug", cobrax.WithRunE(cobra.NoArgs), cobrax.WithArgs(cobra.NoArgs))
--- tools/goctlwt/example/rpc/hi/internal/config/config.go
+++
@@ -1,7 +0,0 @@
-package config
-
-import "github.com/wuntsong-org/go-zero-plus/zrpc"
-
-type Config struct {
-	zrpc.RpcServerConf
-}
